5a745d81b1ce0c9a91dba6a53cbbe3ee64378b80
Mark Connolly <markconnolly@iMac.home>
2016-01-30 17:06:36 -0500

    updated port for modulus migration

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ function start(route, handle) {
     
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT || 8888);
   console.log("Server has started.");
 }
 
